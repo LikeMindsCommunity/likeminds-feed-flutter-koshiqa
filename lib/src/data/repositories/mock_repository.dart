2be68eb2c61ba7dd6c6fcfa2fa03183f3012759d
Suryansh Singh Tomar <suryansh.tomar@likeminds.community>
2022-12-20 13:12:35 +0530

    Used better naming conventions

--- lib/src/data/repositories/mock_repository.dart
+++ lib/src/data/repositories/mock_repository.dart
@@ -1,19 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:feed_sx/src/data/local/local_db.dart';
 import 'package:feed_sx/src/data/models/branding.dart';
+import 'package:feed_sx/src/sdk/mock_sdk.dart';
 
 class MockRepository {
-  Future<Branding> getBrandingData() async {
-    Map<String, dynamic> mockData = {
-      'branding': {
-        'basic': {'primary_colour': '0x00ffffff'},
-        'advanced': {
-          'header_colour': '0x00ffffff',
-          'button_icons_colour': '0x00ffffff',
-          'text_links_colour': '0x00ffffff',
-        },
+  final MockSDK _mockSDK;
+  final LocalDB _localDB;
+  MockRepository({
+    required MockSDK mockSDK,
+    required LocalDB localDB,
+  })  : _mockSDK = mockSDK,
+        _localDB = localDB;
+  Future<Branding?> getCommunityBranding(String communityId) async {
+    await Future.delayed(Duration(seconds: 1));
+    Branding? branding;
+    try {
+      branding = await _mockSDK.getBrandingData();
+    } catch (e) {
+      //TODO
+    }
+    if (branding != null) {
+      _localDB.saveBranding(branding: branding.toEntity());
+    } else {
+      BrandingEntity? brandingEntity = await _localDB.getSavedBranding();
+      if (brandingEntity != null) {
+        branding = Branding.fromEntity(brandingEntity);
       }
-    };
-    BrandingEntity brandingEntity =
-        BrandingEntity.fromJson(mockData['branding']);
-    return Branding.fromEntity(brandingEntity);
+    }
+
+    return branding;
   }
 }
---
+++ lib/src/sdk/mock_sdk.dart
@@ -0,0 +1,19 @@
+import 'package:feed_sx/src/data/models/branding.dart';
+
+class MockSDK {
+  Future<Branding> getBrandingData() async {
+    Map<String, dynamic> mockData = {
+      'branding': {
+        'basic': {'primary_colour': '0x00ffffff'},
+        'advanced': {
+          'header_colour': '0x00ffffff',
+          'button_icons_colour': '0x00ffffff',
+          'text_links_colour': '0x00ffffff',
+        },
+      }
+    };
+    BrandingEntity brandingEntity =
+        BrandingEntity.fromJson(mockData['branding']);
+    return Branding.fromEntity(brandingEntity);
+  }
+}
