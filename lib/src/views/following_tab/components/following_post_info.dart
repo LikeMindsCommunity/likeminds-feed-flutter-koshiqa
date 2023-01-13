549816966ce070d964400a9624bdf9b514bee8b6
Suryansh Singh Tomar <suryansh.tomar@likeminds.community>
2023-01-13 16:55:11 +0530

    Following tab screen

---
+++ lib/src/views/following_tab/components/following_post_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FollowingPostInfo extends StatelessWidget {
+  const FollowingPostInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Text('Hridesh Shrotiya commented on this'),
+    );
+  }
+}
