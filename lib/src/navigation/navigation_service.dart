8ad38caf60f932aaa3f04408b993921820d7336d
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-02-13 23:38:25 +0530

    Remove commented code

--- lib/src/navigation/navigation_service.dart
+++ lib/src/navigation/navigation_service.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class NavigationService {
-//   late final GlobalKey<NavigatorState> navigatorKey;
-
-//   static NavigationService? _instance;
-//   static NavigationService get instance => _instance ??= NavigationService._();
-
-//   NavigationService._();
-
-//   void initialize(GlobalKey<NavigatorState> key) {
-//     navigatorKey = key;
-//   }
-
-//   Future<dynamic> navigateTo(String routeName, {Object? arguments}) {
-//     return navigatorKey.currentState!.pushNamed(
-//       routeName,
-//       arguments: arguments,
-//     );
-//   }
-// }
-
 class NavigationService {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
