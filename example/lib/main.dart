e3051516134daa0eb938fea289d502b730c609b1
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-02-13 21:33:54 +0530

    Making notifications more robust

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,59 +1,60 @@
-import 'package:device_info_plus/device_info_plus.dart';
+import 'package:feed_sx/feed.dart';
 import 'package:feed_example/cred_screen.dart';
 import 'package:feed_example/likeminds_callback.dart';
-import 'package:feed_sx/feed.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 /// First level notification handler
 /// Essential to declare it outside of any class or function as per Firebase docs
+/// Call [LMNotificationHandler.instance.handleNotification] in this function
+/// to handle notifications at the second level (inside the app)
+/// Make sure to call [setupNotifications] before this function
 Future<void> _handleNotification(RemoteMessage message) async {
   print("--- Notification received in LEVEL 1 ---");
-  showNotification(message);
-  await LMNotificationHandler.instance.handleNotification(message);
+  await LMNotificationHandler.instance.handleNotification(message, true);
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_handleNotification);
-  setupLocator(LikeMindsCallback());
+  setupLMFeed(LikeMindsCallback());
   setupNotifications();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return OverlaySupport.global(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const CredScreen(),
-      ),
-    );
-  }
-}
-
 /// Setup notifications
-/// 1. Get device id - [deviceId]
-/// 2. Get FCM token - [setupMessaging]
-/// 3. Register device with LM - [LMNotificationHandler]
-/// 4. Listen for notifications
-/// 5. Handle notifications - [_handleNotification]
+/// 1. Initialize Firebase
+/// 2. Get device id - [deviceId]
+/// 3. Get FCM token - [setupMessaging]
+/// 4. Register device with LM - [LMNotificationHandler]
+/// 5. Listen for FG and BG notifications
+/// 6. Handle notifications - [_handleNotification]
 void setupNotifications() async {
+  await Firebase.initializeApp();
   final devId = await deviceId();
   final fcmToken = await setupMessaging();
-  LMNotificationHandler.instance.init(devId, fcmToken);
+  if (fcmToken == null) {
+    print("FCM token is null or permission declined");
+    return;
+  }
+  LMNotificationHandler.instance.init(deviceId: devId, fcmToken: fcmToken);
+  FirebaseMessaging.onBackgroundMessage(_handleNotification);
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     _handleNotification(message);
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+    print("---The app is opened from a notification---");
+    await LMNotificationHandler.instance.handleNotification(message, false);
+  });
+  FirebaseMessaging.instance
+      .getInitialMessage()
+      .then((RemoteMessage? message) async {
+    if (message != null) {
+      print("---The terminated app is opened from a notification---");
+      await LMNotificationHandler.instance.handleNotification(message, false);
+    }
   });
 }
 
@@ -69,14 +70,15 @@ Future<String> deviceId() async {
   return deviceId.toString();
 }
 
-/// Setup Firebase messaging
+/// Setup Firebase messaging on your app
+/// The UI package needs your Firebase instance to be initialized
 /// 1. Get messaging instance
 /// 2. Get FCM token
 /// 3. Request permission
 /// 4. Return FCM token
-Future<String> setupMessaging() async {
+Future<String?> setupMessaging() async {
   final messaging = FirebaseMessaging.instance;
-  final token = await messaging.getToken();
+  NotificationSettings android = await messaging.getNotificationSettings();
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
     announcement: false,
@@ -86,37 +88,14 @@ Future<String> setupMessaging() async {
     provisional: false,
     sound: true,
   );
-  print('User granted permission: ${settings.authorizationStatus}');
-  print("Token - $token");
-  return token.toString();
-}
 
-/// Show a simple notification using overlay package
-void showNotification(RemoteMessage message) {
-  if (message.data.isNotEmpty) {
-    print("Notification received - ${message.data["sub_title"]}");
-    showSimpleNotification(
-      Text(
-        message.data["sub_title"],
-        style: const TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-        ),
-      ),
-      background: Colors.white,
-      duration: const Duration(seconds: 5),
-      leading: Icon(
-        Icons.notifications,
-        color: Colors.blue.shade400,
-        size: 24,
-      ),
-      trailing: Icon(
-        Icons.close,
-        color: Colors.grey.shade400,
-        size: 18,
-      ),
-      position: NotificationPosition.top,
-      slideDismissDirection: DismissDirection.horizontal,
-    );
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    final token = await messaging.getToken();
+    print('User granted permission: ${settings.authorizationStatus}');
+    print("Token - $token");
+    return token.toString();
+  } else {
+    toast('User declined or has not accepted notification permissions');
+    return null;
   }
 }
--- lib/src/utils/notification_handler.dart
+++ lib/src/utils/notification_handler.dart
@@ -1,12 +1,23 @@
+import 'package:feed_sx/src/navigation/arguments.dart';
+import 'package:feed_sx/src/navigation/navigation_service.dart';
 import 'package:feed_sx/src/services/service_locator.dart';
 import 'package:feed_sx/src/services/likeminds_service.dart';
+import 'package:feed_sx/src/views/comments/all_comments_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 import 'package:likeminds_feed/likeminds_feed.dart';
+import 'package:overlay_support/overlay_support.dart';
 
+/// This class handles all the notification related logic
+/// It registers the device for notifications in the SDK
+/// It handles the notification when it is received and shows it
+/// It routes the notification to the appropriate screen
+/// Since this is a singleton class, it is initialized on the client side
 class LMNotificationHandler {
+  // late final Key notifKey;
+  late final String deviceId;
+  late final String fcmToken;
   int? memberId;
-  String? deviceId;
-  String? fcmToken;
 
   static LMNotificationHandler? _instance;
   static LMNotificationHandler get instance =>
@@ -14,19 +25,30 @@ class LMNotificationHandler {
 
   LMNotificationHandler._();
 
-  void init(
-    String deviceId,
-    String fcmToken,
-  ) {
+  /// Initialize the notification handler
+  /// This is called from the client side
+  /// It initializes the [fcmToken] and the [deviceId]
+  /// It also initializes the [notifKey] which is used to show the notification
+  void init({
+    required String deviceId,
+    required String fcmToken,
+    // String notifKey = "likeminds-notification",
+  }) {
     this.deviceId = deviceId;
     this.fcmToken = fcmToken;
+    // this.notifKey = ValueKey(notifKey);
   }
 
+  /// Register the device for notifications
+  /// This is called from the client side
+  /// It calls the [registerDevice] method of the [LikeMindsService]
+  /// It initializes the [memberId] which is used to route the notification
+  /// If the registration is successful, it prints success message
   void registerDevice(int memberId) async {
     RegisterDeviceRequest request = RegisterDeviceRequest(
-      token: fcmToken!,
+      token: fcmToken,
       memberId: memberId,
-      deviceId: deviceId!,
+      deviceId: deviceId,
     );
     this.memberId = memberId;
     final response = await locator<LikeMindsService>().registerDevice(request);
@@ -37,13 +59,107 @@ class LMNotificationHandler {
     }
   }
 
-  Future<void> handleNotification(RemoteMessage message) async {
+  /// Handle the notification when it is received
+  /// This is called from the client side when notification [message] is received
+  /// and is needed to be handled, i.e. shown and routed to the appropriate screen
+  Future<void> handleNotification(RemoteMessage message, bool show) async {
+    print("--- Notification received in LEVEL 2 ---");
+    if (show && message.data.isNotEmpty) {
+      showNotification(message);
+    } else if (message.data.isNotEmpty) {
+      routeNotification(message);
+    } else if (message.notification != null) {
+      print("Notification data is empty");
+      message.toMap().forEach((key, value) {
+        print("$key: $value");
+      });
+    }
     //TODO: Add logic to handle notification
     // First, check if the message contains a data payload.
     // Second, check if this is a LM notification
     // Third, extract the notification data and routes to the appropriate screen
-    message.data.forEach((key, value) {
+    // message.data.forEach((key, value) {
+    //   print("$key: $value");
+    // });
+    message.toMap().forEach((key, value) {
       print("$key: $value");
     });
+  }
+
+  void routeNotification(RemoteMessage message) async {
+    Map<String, String> queryParams = {};
+    String host = "";
+    if (message.data.isNotEmpty) {
+      final Map<String, dynamic> notifData = message.data;
+      final String category = notifData["category"];
+      final String route = notifData["route"]!;
+
+      if (category.toString().toLowerCase() == "feed") {
+        final Uri routeUri = Uri.parse(route);
+        final Map<String, String> routeParams =
+            routeUri.hasQuery ? routeUri.queryParameters : {};
+        final String routeHost = routeUri.host;
+        host = routeHost;
+        print("The route host is $routeHost");
+        queryParams.addAll(routeParams);
+        queryParams.forEach((key, value) {
+          print("$key: $value");
+        });
+      }
+    }
+    if (host == "post_detail") {
+      final String postId = queryParams["post_id"]!;
+      final GetPostResponse postDetails =
+          await locator<LikeMindsService>().getPost(
+        GetPostRequest(
+          postId: postId,
+          page: 1,
+          pageSize: 10,
+        ),
+      );
+      locator<NavigationService>().navigateTo(
+        AllCommentsScreen.route,
+        arguments: AllCommentsScreenArguments(
+          post: postDetails.post!,
+        ),
+      );
+    }
+  }
+
+  /// Show a simple notification using overlay package
+  /// This is a dismissable notification shown on the top of the screen
+  /// It is shown when the notification is received in foreground
+  void showNotification(RemoteMessage message) {
+    if (message.data.isNotEmpty) {
+      showSimpleNotification(
+        // key: notifKey.runtimeType == ValueKey ? notifKey : const ValueKey(""),
+        GestureDetector(
+          onTap: () {
+            routeNotification(message);
+          },
+          child: Text(
+            message.data["sub_title"],
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        background: Colors.white,
+        duration: const Duration(seconds: 3),
+        leading: Icon(
+          Icons.notifications,
+          color: Colors.blue.shade400,
+          size: 24,
+        ),
+        trailing: Icon(
+          Icons.swipe_right_outlined,
+          color: Colors.grey.shade400,
+          size: 18,
+        ),
+        position: NotificationPosition.top,
+        slideDismissDirection: DismissDirection.horizontal,
+      );
+    }
   }
 }
