3f137c2a8127a048309ba185198543273f4de8a7
Tyagi Anurag <anurag.tyagi@likeminds.community>
2023-02-24 13:39:01 +0530

    Fixing profile picture issue

--- lib/src/views/feed/components/post/post_header.dart
+++ lib/src/views/feed/components/post/post_header.dart
@@ -1,3 +1,4 @@
+import 'package:feed_sx/src/widgets/profile_picture.dart';
 import 'package:likeminds_feed/likeminds_feed.dart';
 import 'package:feed_sx/src/utils/constants/ui_constants.dart';
 import 'package:feed_sx/src/utils/utils.dart';
@@ -24,46 +25,24 @@ class PostHeader extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
         children: [
-          Container(
-            height: 48,
-            width: 48,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: user.name.isNotEmpty ? kPrimaryColor : kGrey3Color,
-              image: user.imageUrl.isNotEmpty
-                  ? DecorationImage(
-                      image: NetworkImage(user.imageUrl),
-                    )
-                  : null,
-            ),
-            child: user.imageUrl.isEmpty
-                ? Center(
-                    child: Text(
-                      user.name.isNotEmpty ? user.name[0].toUpperCase() : "",
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  )
-                : null,
-          ),
+          ProfilePicture(user: user),
           kHorizontalPaddingLarge,
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
-                  Text(user.name.isNotEmpty ? user.name : "Deleted user",
-                      style: TextStyle(
-                        fontSize: kFontMedium,
-                        color: kGrey1Color,
-                        fontWeight: FontWeight.w500,
-                        fontStyle: user.name.isNotEmpty
-                            ? FontStyle.normal
-                            : FontStyle.italic,
-                      )),
+                  Text(
+                    user.name.isNotEmpty ? user.name : "Deleted user",
+                    style: TextStyle(
+                      fontSize: kFontMedium,
+                      color: kGrey1Color,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: user.name.isNotEmpty
+                          ? FontStyle.normal
+                          : FontStyle.italic,
+                    ),
+                  ),
                   kHorizontalPaddingLarge,
                 ],
               ),
