35ecb82f6349209471c2eab31467a17c80829bc8
Tyagi Anurag <anurag.tyagi@likeminds.community>
2023-03-29 19:57:49 +0530

    Added overlay on video

--- lib/src/views/feed/components/post/post_media/post_video.dart
+++ lib/src/views/feed/components/post/post_media/post_video.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:feed_sx/src/utils/constants/ui_constants.dart';
@@ -22,6 +23,9 @@ class _PostVideoState extends State<PostVideo>
     with AutomaticKeepAliveClientMixin {
   late final VideoPlayerController videoPlayerController;
   late ChewieController chewieController;
+  bool _onTouch = true;
+
+  Timer? _timer;
 
   @override
   void dispose() {
@@ -54,7 +58,7 @@ class _PostVideoState extends State<PostVideo>
       autoPlay: false,
       looping: false,
       showOptions: false,
-      showControls: true,
+      showControls: false,
       allowPlaybackSpeedChanging: false,
       allowMuting: false,
       allowFullScreen: false,
@@ -62,7 +66,6 @@ class _PostVideoState extends State<PostVideo>
       showControlsOnInitialize: false,
     );
     await videoPlayerController.initialize();
-    videoPlayerController.play();
   }
 
   @override
@@ -72,21 +75,80 @@ class _PostVideoState extends State<PostVideo>
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const PostShimmer();
-          }
-          return Container(
-            color: kWhiteColor,
-            alignment: Alignment.center,
-            child: AspectRatio(
-              aspectRatio:
-                  chewieController.videoPlayerController.value.aspectRatio,
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: Chewie(
-                  controller: chewieController,
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return StatefulBuilder(builder: (context, setChildState) {
+              return Stack(children: [
+                GestureDetector(
+                  onTap: () {
+                    setChildState(() {
+                      _onTouch = !_onTouch;
+                    });
+                  },
+                  child: Container(
+                    width: widget.width,
+                    height: widget.width,
+                    color: kWhiteColor,
+                    alignment: Alignment.center,
+                    child: AspectRatio(
+                      aspectRatio: chewieController
+                          .videoPlayerController.value.aspectRatio,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: Chewie(
+                          controller: chewieController,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          );
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Visibility(
+                    visible: _onTouch,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(const CircleBorder(
+                              side: BorderSide(color: Colors.white))),
+                        ),
+                        child: Icon(
+                          videoPlayerController.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          _timer?.cancel();
+
+                          // pause while video is playing, play while video is pausing
+                          setChildState(() {
+                            videoPlayerController.value.isPlaying
+                                ? videoPlayerController.pause()
+                                : videoPlayerController.play();
+                          });
+
+                          // Auto dismiss overlay after 1 second
+                          _timer =
+                              Timer.periodic(Duration(milliseconds: 1000), (_) {
+                            setChildState(() {
+                              _onTouch = false;
+                            });
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                )
+              ]);
+            });
+          } else {
+            return const SizedBox();
+          }
         });
   }
 
