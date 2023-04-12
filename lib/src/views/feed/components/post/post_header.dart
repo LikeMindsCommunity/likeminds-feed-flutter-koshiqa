33178d0e6b9fb0722fdd1339479861540aedb85c
Tyagi Anurag <anurag.tyagi@likeminds.community>
2023-04-12 09:58:33 +0530

    Fixed custom title issue

--- lib/src/views/feed/components/post/post_header.dart
+++ lib/src/views/feed/components/post/post_header.dart
@@ -29,114 +29,130 @@ class PostHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     removeReportIntegration();
     final bool isEdited = postDetails.createdAt != postDetails.updatedAt;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        children: [
-          ProfilePicture(user: user),
-          kHorizontalPaddingLarge,
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+      child: SizedBox(
+        width: screenSize.width - 32,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ProfilePicture(user: user),
+            kHorizontalPaddingLarge,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(
-                    user.isDeleted == null || !user.isDeleted!
-                        ? user.name
-                        : "Deleted user",
-                    maxLines: 1,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: kFontMedium,
-                      color: kGrey1Color,
-                      fontWeight: FontWeight.w500,
-                      fontStyle: user.name.isNotEmpty
-                          ? FontStyle.normal
-                          : FontStyle.italic,
-                    ),
-                  ),
-                  kHorizontalPaddingMedium,
-                  (user.customTitle == null || user.customTitle!.isEmpty) ||
-                          (user.isDeleted != null && user.isDeleted!)
-                      ? const SizedBox()
-                      : Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(3.0),
-                            color: kPrimaryColor,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Text(
-                              user.customTitle!.isNotEmpty
-                                  ? user.customTitle!
-                                  : "",
-                              style: TextStyle(
-                                fontSize: kFontSmall,
-                                color: kWhiteColor,
-                                fontWeight: FontWeight.w600,
-                                fontStyle: user.name.isNotEmpty
-                                    ? FontStyle.normal
-                                    : FontStyle.italic,
-                              ),
-                            ),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          user.isDeleted == null || !user.isDeleted!
+                              ? user.name
+                              : "Deleted user",
+                          maxLines: 1,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: kFontMedium,
+                            color: kGrey1Color,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: user.name.isNotEmpty
+                                ? FontStyle.normal
+                                : FontStyle.italic,
                           ),
                         ),
-                  // kHorizontalPaddingLarge,x/
+                      ),
+                      kHorizontalPaddingMedium,
+                      (user.customTitle == null || user.customTitle!.isEmpty) ||
+                              (user.isDeleted != null && user.isDeleted!)
+                          ? const SizedBox()
+                          : Row(mainAxisSize: MainAxisSize.min, children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(3.0),
+                                  color: kPrimaryColor,
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Text(
+                                    user.customTitle!.isNotEmpty
+                                        ? user.customTitle!
+                                        : "",
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                      fontSize: kFontSmall,
+                                      color: kWhiteColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontStyle: user.name.isNotEmpty
+                                          ? FontStyle.normal
+                                          : FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]),
+                    ],
+                  ),
+                  kVerticalPaddingSmall,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        postDetails.createdAt.timeAgo(),
+                        style: const TextStyle(
+                          fontSize: kFontSmall,
+                          color: kGrey3Color,
+                        ),
+                      ),
+                      kHorizontalPaddingXSmall,
+                      Text(
+                        isEdited ? '·' : '',
+                        style: const TextStyle(
+                          fontSize: kFontSmall,
+                          color: kGrey3Color,
+                        ),
+                      ),
+                      kHorizontalPaddingXSmall,
+                      Text(
+                        isEdited ? 'Edited' : '',
+                        style: const TextStyle(
+                          fontSize: kFontSmall,
+                          color: kGrey3Color,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
-              kVerticalPaddingSmall,
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    postDetails.createdAt.timeAgo(),
-                    style: const TextStyle(
-                      fontSize: kFontSmall,
-                      color: kGrey3Color,
-                    ),
-                  ),
-                  kHorizontalPaddingXSmall,
-                  Text(
-                    isEdited ? '·' : '',
-                    style: const TextStyle(
-                      fontSize: kFontSmall,
-                      color: kGrey3Color,
-                    ),
-                  ),
-                  kHorizontalPaddingXSmall,
-                  Text(
-                    isEdited ? 'Edited' : '',
-                    style: const TextStyle(
-                      fontSize: kFontSmall,
-                      color: kGrey3Color,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          const Spacer(),
-          postDetails.isPinned
-              ? SvgPicture.asset(
-                  "packages/feed_sx/assets/icons/pin.svg",
-                  color: kGrey3Color,
-                  height: 20,
-                  width: 20,
-                  fit: BoxFit.cover,
-                )
-              : const SizedBox(),
-          kHorizontalPaddingLarge,
-          menuItems.isNotEmpty
-              ? DropdownOptions(
-                  menuItems: menuItems,
-                  postDetails: postDetails,
-                  refresh: refresh,
-                )
-              : const SizedBox()
-        ],
+            ),
+            kHorizontalPaddingMedium,
+            Row(
+              children: [
+                postDetails.isPinned
+                    ? SvgPicture.asset(
+                        "packages/feed_sx/assets/icons/pin.svg",
+                        color: kGrey3Color,
+                        height: 20,
+                        width: 20,
+                        fit: BoxFit.cover,
+                      )
+                    : const SizedBox(),
+                kHorizontalPaddingLarge,
+                menuItems.isNotEmpty
+                    ? DropdownOptions(
+                        menuItems: menuItems,
+                        postDetails: postDetails,
+                        refresh: refresh,
+                      )
+                    : const SizedBox()
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
